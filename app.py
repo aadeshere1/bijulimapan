480dffc921f3c383ec87ec479f04cc23e9c102ee
Aadesh <aadeshere1@gmail.com>
2017-10-16 11:09:12 +0545

    add mailer

--- app.py
+++ app.py
@@ -2,15 +2,33 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, redirect, render_template, url_for
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
-
+from flask_mail import Mail
 import os
 
+mail = Mail()
 app = Flask(__name__)
+
+app.config.update(
+        DEBUG=True,
+        MAIL_SERVER='smtp.gmail.com',
+        MAIL_PORT=465,
+        MAIL_USE_TLS=False,
+        MAIL_USE_SSL=True,
+        MAIL_USERNAME=os.environ['GMAIL_USERNAME'],
+        MAIL_PASSWORD=os.environ['GMAIL_PASSWORD']
+        )
+
+mail.init_app(app)
+
 if os.environ['ENV'] == 'production':
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/flaskmovie'
+    app.config['SECRET_KEY'] = 'super-secret'
 
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_PASSWORD_SALT'] = 'MMMMM...SALTY'
 app.debug = True
 db = SQLAlchemy(app)
 
@@ -37,21 +55,20 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
 #create a user to test
-@app.before_first_request
-def create_user():
-    db.create_all()
-    user_datastore.create_user(email='aadesh@gmail.com', password='password')
-    db.session.commit()
+#@app.before_first_request
+#def create_user():
+#    db.create_all()
+#    user_datastore.create_user(email='aadesh@gmail.com', password='password')
+#    db.session.commit()
 
 @app.route('/')
 def index():
-    allUser = User.query.all()
-    oneUser = User.query.filter_by(username="aadesh").first()
-    return render_template('add_user.html', allUser=allUser, oneUser=oneUser)
+    return render_template('add_user.html')
 
-@app.route('/profile/<username>')
-def profile(username):
-    user = User.query.filter_by(username=username).first()
+@app.route('/profile/<email>')
+@login_required
+def profile(email):
+    user = User.query.filter_by(email=email).first()
     if user is None:
         return redirect(url_for('index'))
     else:
